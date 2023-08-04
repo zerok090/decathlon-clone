6c232ac21db24551b8fe18a04e96c894c859d4b7
Daniel Kadar <daniel.kadar@teamrockstars.nl>
2023-08-04 10:09:28 +0200

    start card component for products

---
+++ src/components/ProductCard.svelte
@@ -0,0 +1,26 @@
+<script>
+	import Rating from "$components/Rating.svelte";
+
+	/** @type {string} */
+	export let id;
+	/** @type {string} */
+	export let title;
+	/** @type {string} */
+	export let description;
+	/** @type {string} */
+	export let image;
+	/** @type {number} */
+	export let price;
+	/** @type {{rate: number, count: number}} */
+	export let rating;
+	/** @type {string} */
+	export let category;
+</script>
+
+<a href="/products/{id}" class="flex flex-col items-center">
+	<p>{price}</p>
+	<img src={image} alt="img" width="160" height="160" class="aspect-square object-contain" />
+	<p>{title}</p>
+	<p>{description}</p>
+	<div><Rating rating={rating.rate} /> ({rating.count})</div>
+</a>
---
+++ src/components/Rating.svelte
@@ -0,0 +1,21 @@
+<script>
+	import Star from '~icons/fa/star';
+	import StarO from '~icons/fa/star-o';
+	import StarHalfO from '~icons/fa/star-half-o';
+
+	/** @type {number} */
+	export let rating;
+	const numberOfStars = 5;
+</script>
+
+<div class="flex text-xs text-yellow-400">
+	{#each { length: numberOfStars } as _, i}
+		{#if rating - i - 1 >= 0}
+			<Star />
+		{:else if rating - i - 1 >= -0.5}
+			<StarHalfO />
+		{:else}
+			<StarO />
+		{/if}
+	{/each}
+</div>
