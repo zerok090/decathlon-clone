1a0737ecbda7a309acc6cb3f6f2fd9376fb62a9a
Daniel Kadar <daniel.kadar@teamrockstars.nl>
2023-08-06 14:10:12 +0200

    stolen footer content decathlon site

--- src/components/Footer.svelte
+++ src/components/Footer.svelte
@@ -1,1 +1,361 @@
-<footer>Footer</footer>
+<!-- <footer>
+	<section class="container extras">
+		<div class="extra-items">
+			<div class="">
+				<span class="title--4th">Shop met vertrouwen</span>
+				<div class="links grid" role="list">
+					<div role="listitem" class="">
+						<span class="link"
+							><img
+								data-src="/render/ideal.nl.b2fd39b02f901374a0b3.svg"
+								aria-hidden="true"
+								alt="iDeal"
+								style="aspect-ratio: 40 / 40; width:4rem;"
+								class=" lz-ldd"
+								src="/render/ideal.nl.b2fd39b02f901374a0b3.svg"
+								data-sourced="true"
+								width="40"
+								height="40"
+							/></span
+						>
+					</div>
+					<div role="listitem" class="">
+						<span class="link"
+							><img
+								data-src="/render/visa.4de8e1f0a7d44c463aa0.svg"
+								aria-hidden="true"
+								alt="Visa"
+								style="aspect-ratio: 60 / 20; width:6rem;"
+								class=" lz-ldd"
+								src="/render/visa.4de8e1f0a7d44c463aa0.svg"
+								data-sourced="true"
+								width="60"
+								height="20"
+							/></span
+						>
+					</div>
+					<div role="listitem" class="">
+						<span class="link"
+							><img
+								data-src="/render/mastercard.b7560b8e172bfcfdcb83.svg"
+								aria-hidden="true"
+								alt="MasterCard"
+								style="aspect-ratio: 45 / 40; width:4.5rem;"
+								class=" lz-ldd"
+								src="/render/mastercard.b7560b8e172bfcfdcb83.svg"
+								data-sourced="true"
+								width="45"
+								height="40"
+							/></span
+						>
+					</div>
+					<div role="listitem" class="">
+						<span class="link"
+							><img
+								data-src="/render/riverty-checkout-logo.nl.0a02be3810026b8c4ab6.svg"
+								aria-hidden="true"
+								alt="Afterpay"
+								style="aspect-ratio: 72 / 40; width:7.2rem;"
+								class=" lz-ldd"
+								src="/render/riverty-checkout-logo.nl.0a02be3810026b8c4ab6.svg"
+								data-sourced="true"
+								width="72"
+								height="40"
+							/></span
+						>
+					</div>
+					<div role="listitem" class="">
+						<span class="link"
+							><img
+								data-src="/render/postnl.nl.e849328706e0e51da96a.svg"
+								aria-hidden="true"
+								alt="PostNL"
+								style="aspect-ratio: 40 / 40; width:4rem;"
+								class=" lz-ldd"
+								src="/render/postnl.nl.e849328706e0e51da96a.svg"
+								data-sourced="true"
+								width="40"
+								height="40"
+							/></span
+						>
+					</div>
+					<div role="listitem" class="">
+						<span class="link"
+							><img
+								data-src="/render/dynalogic.nl.b7ad8bb9e888f03a97ed.svg"
+								aria-hidden="true"
+								alt="Dynalogic"
+								style="aspect-ratio: 72 / 40; width:7.2rem;"
+								class=" lz-ldd"
+								src="/render/dynalogic.nl.b7ad8bb9e888f03a97ed.svg"
+								data-sourced="true"
+								width="72"
+								height="40"
+							/></span
+						>
+					</div>
+					<div role="listitem" class="">
+						<span class="link"
+							><img
+								data-src="/render/thuiswinkel-waarborg.nl.5a128872a48c77367bd6.svg"
+								aria-hidden="true"
+								alt="Thuiswinkel Waarborg"
+								style="aspect-ratio: 72 / 40; width:7.2rem;"
+								class=" lz-ldd"
+								src="/render/thuiswinkel-waarborg.nl.5a128872a48c77367bd6.svg"
+								data-sourced="true"
+								width="72"
+								height="40"
+							/></span
+						>
+					</div>
+					<div role="listitem" class="">
+						<span class="link"
+							><img
+								data-src="/render/lemonway-logo.nl.8fb217e17d091bb13cb3.svg"
+								aria-hidden="true"
+								alt="Lemonway"
+								style="aspect-ratio: 102 / 24; width:10.2rem;"
+								class=" lz-ldd"
+								src="/render/lemonway-logo.nl.8fb217e17d091bb13cb3.svg"
+								data-sourced="true"
+								width="102"
+								height="24"
+							/></span
+						>
+					</div>
+				</div>
+			</div>
+			<div class="">
+				<span class="title--4th">Download onze app</span>
+				<div class="links" role="list">
+					<div role="listitem" class="">
+						<a
+							class="link mobile"
+							data-track-footer="extralink-App Store"
+							href="https://app.adjust.com/y3iuba0?deep_link=dktappmobile%3A%2F%2Fhome&amp;campaign=website&amp;adgroup=website_banner_footer_ios&amp;creative=Generic&amp;redirect_macos=https://apps.apple.com/nl/app/decathlon-shopping/id1168607403&amp;fallback=https://apps.apple.com/nl/app/decathlon-shopping/id1168607403"
+							target="_blank"
+							rel="noreferrer noopener"
+							aria-label="App Store"
+							><img
+								data-src="/render/app-store-button.nl.65a6ca13bc893bd59e82.svg"
+								aria-hidden="true"
+								alt="App Store"
+								style="aspect-ratio: 133 / 45; width:13.3rem;"
+								class=""
+								width="133"
+								height="45"
+							/></a
+						>
+					</div>
+					<div role="listitem" class="">
+						<a
+							class="link desktop"
+							data-track-footer="extralink-App Store"
+							href="https://app.adjust.com/y3iuba0?deep_link=dktappmobile%3A%2F%2Fhome&amp;campaign=website&amp;adgroup=website_banner_footer_ios&amp;creative=Generic&amp;redirect_macos=https://apps.apple.com/nl/app/decathlon-shopping/id1168607403&amp;fallback=https://apps.apple.com/nl/app/decathlon-shopping/id1168607403"
+							target="_blank"
+							rel="noreferrer noopener"
+							aria-label="App Store"
+							><img
+								data-src="/render/app-store-logo.nl.d8aeae9c9a0066e63d87.svg"
+								aria-hidden="true"
+								alt="App Store"
+								style="aspect-ratio: 34 / 34; width:3.4rem;"
+								class=" lz-ldd"
+								src="/render/app-store-logo.nl.d8aeae9c9a0066e63d87.svg"
+								data-sourced="true"
+								width="34"
+								height="34"
+							/></a
+						>
+					</div>
+					<div role="listitem" class="">
+						<a
+							class="link mobile"
+							data-track-footer="extralink-Google Play"
+							href="https://app.adjust.com/y3iuba0?deep_link=dktappmobile%3A%2F%2Fhome&amp;campaign=website&amp;adgroup=website_banner_footer_android&amp;creative=Generic&amp;redirect_macros=https://play.google.com/store/apps/details?id=com.decathlon.app&amp;fallback=https://play.google.com/store/apps/details?id=com.decathlon.app"
+							target="_blank"
+							rel="noreferrer noopener"
+							aria-label="Google Play"
+							><img
+								data-src="/render/google-play-button.nl.e75577f02b3cab15ef00.svg"
+								aria-hidden="true"
+								alt="Google Play"
+								style="aspect-ratio: 150 / 45; width:15rem;"
+								class=""
+								width="150"
+								height="45"
+							/></a
+						>
+					</div>
+					<div role="listitem" class="">
+						<a
+							class="link desktop"
+							data-track-footer="extralink-Google Play"
+							href="https://app.adjust.com/y3iuba0?deep_link=dktappmobile%3A%2F%2Fhome&amp;campaign=website&amp;adgroup=website_banner_footer_android&amp;creative=Generic&amp;redirect_macros=https://play.google.com/store/apps/details?id=com.decathlon.app&amp;fallback=https://play.google.com/store/apps/details?id=com.decathlon.app"
+							target="_blank"
+							rel="noreferrer noopener"
+							aria-label="Google Play"
+							><img
+								data-src="/render/google-play-logo.nl.c78c476ffbe0ba53e26e.svg"
+								aria-hidden="true"
+								alt="Google Play"
+								style="aspect-ratio: 33 / 34; width:3.3rem;"
+								class=" lz-ldd"
+								src="/render/google-play-logo.nl.c78c476ffbe0ba53e26e.svg"
+								data-sourced="true"
+								width="33"
+								height="34"
+							/></a
+						>
+					</div>
+				</div>
+			</div>
+			<div class="">
+				<div class="links" role="list">
+					<div role="listitem" class="">
+						<span class="link award"
+							><img
+								data-src="/render/WA-NL-Sport-2020-2021.nl.9eeb55974c284e1a6733.svg"
+								aria-hidden="true"
+								alt="Webshop awards Sport"
+								style="aspect-ratio: 100 / 191; width:10rem;"
+								class=" lz-ldd"
+								src="/render/WA-NL-Sport-2020-2021.nl.9eeb55974c284e1a6733.svg"
+								data-sourced="true"
+								width="100"
+								height="191"
+							/></span
+						>
+					</div>
+					<div role="listitem" class="">
+						<span class="link award"
+							><img
+								data-src="/render/WA-NL-Outdoor-2020-2021.nl.4dde506a1f01fe90e1be.svg"
+								aria-hidden="true"
+								alt="Webshop awards Outdoor"
+								style="aspect-ratio: 100 / 189; width:10rem;"
+								class=" lz-ldd"
+								src="/render/WA-NL-Outdoor-2020-2021.nl.4dde506a1f01fe90e1be.svg"
+								data-sourced="true"
+								width="100"
+								height="189"
+							/></span
+						>
+					</div>
+					<div role="listitem" class="">
+						<span class="link award"
+							><img
+								data-src="/render/shopping-awards.nl.baa88331375d18c78898.svg"
+								aria-hidden="true"
+								alt="Shopping award"
+								style="aspect-ratio: 100 / 146; width:10rem;"
+								class=" lz-ldd"
+								src="/render/shopping-awards.nl.baa88331375d18c78898.svg"
+								data-sourced="true"
+								width="100"
+								height="146"
+							/></span
+						>
+					</div>
+				</div>
+			</div>
+			<div class="">
+				<span class="title--4th title--alt">Countries and areas</span>
+				<label for="site-country-selection" class="vh">Countries and areas</label>
+				<div class="select svelte-1az1rrf" translate="no">
+					<select
+						aria-hidden="true"
+						id="site-country-selection"
+						name=""
+						tabindex="-1"
+						class="vh svelte-1az1rrf"
+						><option class="svelte-1az1rrf" value="">48 Countries and areas</option><option
+							value="https://www.decathlon.co.za/"
+							>Afrique du Sud
+						</option><option value="https://www.decathlon.de/">Allemagne </option><option
+							value="https://www.decathlon.at/"
+							>Autriche
+						</option><option value="https://www.decathlon.be/">Belgique </option><option
+							value="https://www.decathlon.com.br/"
+							>Brésil
+						</option><option value="https://www.decathlon.bg/">Bulgarie </option><option
+							value="https://www.decathlon.ca/"
+							>Canada
+						</option><option value="https://www.decathlon.com.cn/">Chine </option><option
+							value="https://www.decathlon.hr/"
+							>Croatie
+						</option><option value="https://www.decathlon.dk/">Danemark </option><option
+							value="https://www.decathlon.es/"
+							>Espagne
+						</option><option value="https://www.decathlon.ee/">Estonie </option><option
+							value="https://www.decathlon.fi/"
+							>Finlande
+						</option><option value="https://www.decathlon.fr/">France </option><option
+							value="https://www.decathlon.co.uk/"
+							>Grande-Bretagne
+						</option><option value="https://www.decathlon.gp/">Guadeloupe </option><option
+							value="https://www.decathlon.com.hk/"
+							>Hong-Kong
+						</option><option value="https://www.decathlon.hu/">Hongrie </option><option
+							value="https://www.decathlon.in/"
+							>Inde
+						</option><option value="https://www.decathlon.co.id/">Indonésie </option><option
+							value="https://www.decathlon.ie/"
+							>Irlande
+						</option><option value="https://www.decathlon.is/">Islande </option><option
+							value="https://www.decathlon.it/"
+							>Italie
+						</option><option value="https://www.decathlon.co.jp/">Japon </option><option
+							value="https://www.decathlon.lv/"
+							>Lettonie
+						</option><option value="https://www.decathlon.lt/">Lituanie </option><option
+							value="https://www.decathlon.lu/"
+							>Luxembourg
+						</option><option value="https://www.decathlon.my/">Malaisie </option><option
+							value="https://www.decathlon.ma/"
+							>Maroc
+						</option><option value="https://www.decathlon.mq/">Martinique </option><option
+							value="https://www.decathlon.com.mx/"
+							>Mexique
+						</option><option value="https://www.decathlon.nl/">Pays-Bas </option><option
+							value="https://www.decathlon.ph/"
+							>Philippines
+						</option><option value="https://www.decathlon.pl/">Pologne </option><option
+							value="https://www.decathlon.pt/"
+							>Portugal
+						</option><option value="https://www.decathlon.cz/">République-Tchèque </option><option
+							value="https://www.decathlon.re/"
+							>Réunion
+						</option><option value="https://www.decathlon.ro/">Roumanie </option><option
+							value="https://www.decathlon.ru/"
+							>Russie
+						</option><option value="https://www.decathlon.sg/">Singapour </option><option
+							value="https://www.decathlon.sk/"
+							>Slovaquie
+						</option><option value="https://www.decathlon.si/">Slovénie </option><option
+							value="https://www.decathlon.se/"
+							>Suède
+						</option><option value="https://www.decathlon.ch/">Suisse </option><option
+							value="https://www.decathlon.co.th/"
+							>Taïlande
+						</option><option value="https://www.decathlon.tw/">Taïwan </option><option
+							value="https://www.decathlon.com.tr/"
+							>Turquie
+						</option><option value="https://www.decathlon.ua/uk/">Ukraine </option></select
+					>
+					<button type="button" tabindex="0" class="svelte-1az1rrf"
+						><span class="label-container svelte-1az1rrf">48 Countries and areas</span>
+						<span class="icon-container svelte-1az1rrf"
+							><svg aria-hidden="true" class="svg-icon" viewBox="0 0 32 32"
+								><path
+									d="M16 20.586L28.293 8.293a1 1 0 011.414 1.414l-13 13a.997.997 0 01-1.414 0l-13-13a1 1 0 011.414-1.414L16 20.586z"
+								/></svg
+							></span
+						></button
+					>
+				</div>
+			</div>
+		</div>
+	</section>
+</footer> -->
