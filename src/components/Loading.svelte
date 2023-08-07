7928ea76c8429b2ca464c7ff21503ff52ec28455
Daniel Kadar <daniel.kadar@teamrockstars.nl>
2023-08-07 17:19:56 +0200

    basic loading component

---
+++ src/components/Loading.svelte
@@ -0,0 +1,27 @@
+<script>
+</script>
+
+<div class="w-full flex justify-center items-center">
+	<div class="loader" />
+</div>
+
+<style>
+	.loader {
+        border: 5px solid white;
+		color: black;
+		border-top: 5px solid #555;
+		border-radius: 50%;
+		width: 50px;
+		height: 50px;
+		animation: spin 1s linear infinite;
+	}
+
+	@keyframes spin {
+		0% {
+			transform: rotate(0deg);
+		}
+		100% {
+			transform: rotate(360deg);
+		}
+	}
+</style>
