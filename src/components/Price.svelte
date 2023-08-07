dd040f69e5a1f6ff6569e8dfc7a3c18e8453b5f9
Daniel Kadar <daniel.kadar@teamrockstars.nl>
2023-08-07 17:20:59 +0200

    moved price to its own component from product card center rating in container

---
+++ src/components/Price.svelte
@@ -0,0 +1,12 @@
+<script>
+    /** @type {number} */
+    export let price;
+	const EURO = new Intl.NumberFormat('nl-NL', {
+		style: 'currency',
+		currency: 'EUR'
+	});
+</script>
+
+<div class="p-1 px-2 text-base rounded bg-yellow-300">
+	<strong>{EURO.format(price)}</strong>
+</div>
--- src/components/ProductCard.svelte
+++ src/components/ProductCard.svelte
@@ -1,5 +1,6 @@
 <script>
 	import Rating from '$components/Rating.svelte';
+	import Price from '$components/Price.svelte';
 
 	/** @type {string} */
 	export let id;
@@ -15,11 +16,6 @@
 	export let rating;
 	/** @type {string} */
 	export let category;
-
-	const EURO = new Intl.NumberFormat('nl-NL', {
-		style: 'currency',
-		currency: 'EUR'
-	});
 </script>
 
 <a
@@ -27,8 +23,8 @@
 	class="flex gap-1 justify-center relative flex-col h-[290px] items-center p-3 px-4 text-sm border-b border-r border-gray-200 border-solid"
 >
 	<img src={image} alt="img" width="160" height="160" class="aspect-square object-contain" />
-	<div class="sm:absolute top-1 right-1 p-1 px-2 text-base rounded bg-yellow-300">
-		<strong>{EURO.format(price)}</strong>
+	<div class="sm:absolute top-1 right-1">
+		<Price price={price} />
 	</div>
 	<strong class="text-center">{title}</strong>
 	<div class="flex items-center gap-2 text-xs mt-1">
