502dddd869d4141d34e801ad0c7a3363df1ee3c6
Daniel Kadar <daniel.kadar@teamrockstars.nl>
2023-08-07 17:21:28 +0200

    product detail page loading animation , some styling of data product

--- src/routes/product/[id]/+page.svelte
+++ src/routes/product/[id]/+page.svelte
@@ -1,14 +1,34 @@
 <script>
-    /** @type {import('./$types').PageData} */
-    export let data;
+	import Loading from '$components/Loading.svelte';
+	import Price from '$components/Price.svelte';
+	import Rating from '$components/Rating.svelte';
+
+	/** @type {import('./$types').PageData} */
+	export let data;
 </script>
 
 {#await data.item.promise}
-  [some nice loading animation]
+	<Loading />
 {:then item}
-  {#if item}
-    <ul>
-      {item.title}
-    </ul>
-  {/if}
+	<article>
+		<div class="w-full relative flex flex-col items-center">
+			<img
+				class="max-w-[20rem] max-h-[20rem] aspect-square object-contain"
+				src={item.image}
+				alt={item.title}
+			/>
+		</div>
+		<div>
+			<div>{item.title}</div>
+			<div class="max-w-fit"><Price price={item.price} /></div>
+			<div class="flex gap-2 items-center text-xs">
+				<Rating rating={item.rating.rate} />
+				<strong class="flex items-center gap-2 h-full">
+					<div class="text-sm">{item.rating.rate}/5</div>
+					<a href="#reviews"><u>Bekijk alle {item.rating.count} reviews</u></a>
+				</strong>
+			</div>
+			<div>{item.description}</div>
+		</div>
+	</article>
 {/await}
