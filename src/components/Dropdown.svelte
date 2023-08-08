d5282d6c2d9fc1de97c11a1cb6297be09c84c8bd
Daniel Kadar <daniel.kadar@teamrockstars.nl>
2023-08-08 13:48:58 +0200

    mobile first version product detail

---
+++ src/components/Dropdown.svelte
@@ -0,0 +1,32 @@
+<script>
+	import Down from '~icons/fa/chevron-down';
+	import Up from '~icons/fa/chevron-up';
+
+	import { slide } from 'svelte/transition';
+	import { quadInOut } from 'svelte/easing';
+
+	/** @type {string} */
+	export let title;
+
+	export let open = false;
+
+	function toggle() {
+		open = !open;
+	}
+</script>
+
+<section class="border-t border-gray-300 p-2 px-4">
+	<button class="flex w-full flex-row justify-between items-center" on:click={toggle}>
+		<h1 class="text-xl font-bold">{title}</h1>
+		{#if open}
+			<Up />
+		{:else}
+			<Down />
+		{/if}
+	</button>
+	{#if open}
+		<div transition:slide={{ delay: 0, duration: 300, easing: quadInOut, axis: 'y' }}>
+			<slot />
+		</div>
+	{/if}
+</section>
--- src/routes/product/[id]/+page.svelte
+++ src/routes/product/[id]/+page.svelte
@@ -1,7 +1,10 @@
 <script>
+	import Dropdown from '$components/Dropdown.svelte';
 	import Loading from '$components/Loading.svelte';
 	import Price from '$components/Price.svelte';
 	import Rating from '$components/Rating.svelte';
+
+	import { cart } from '$stores/cartStore';
 
 	/** @type {import('./$types').PageData} */
 	export let data;
@@ -10,25 +13,69 @@
 {#await data.item.promise}
 	<Loading />
 {:then item}
-	<article>
-		<div class="w-full relative flex flex-col items-center">
-			<img
-				class="max-w-[20rem] max-h-[20rem] aspect-square object-contain"
-				src={item.image}
-				alt={item.title}
-			/>
-		</div>
-		<div>
-			<div>{item.title}</div>
-			<div class="max-w-fit"><Price price={item.price} /></div>
-			<div class="flex gap-2 items-center text-xs">
-				<Rating rating={item.rating.rate} />
-				<strong class="flex items-center gap-2 h-full">
-					<div class="text-sm">{item.rating.rate}/5</div>
-					<a href="#reviews"><u>Bekijk alle {item.rating.count} reviews</u></a>
-				</strong>
+	<div>
+		<article class="flex flex-col gap-2">
+			<div class="w-full relative flex flex-col items-center">
+				<img
+					class="max-w-[20rem] max-h-[20rem] aspect-square object-contain"
+					src={item.image}
+					alt={item.title}
+				/>
 			</div>
-			<div>{item.description}</div>
+			<div class="flex text-sm flex-col gap-2 p-2">
+				<h1 class="font-bold text-base">{item.title}</h1>
+				<div class="max-w-fit"><Price price={item.price} /></div>
+				<div class="flex gap-2 items-center text-xs">
+					<Rating rating={item.rating.rate} />
+					<strong class="flex items-center gap-2 h-full">
+						<div class="text-sm">{item.rating.rate}/5</div>
+						<a class="underline text-center" href="#reviews"
+							>Bekijk alle {item.rating.count} reviews</a
+						>
+					</strong>
+				</div>
+				<div>{item.description}</div>
+				<button
+					on:click={() => cart.addItem(item.id)}
+					class="font-bold text-sm uppercase w-full bg-yellow-300 p-3 hover:bg-yellow-400 transition-colors ease-linear"
+					>Toevoegen aan winkelwagen</button
+				>
+			</div>
+		</article>
+		<div class="flex flex-col last:border-b">
+			<Dropdown title="Aanbevolen artikelen">
+				<div>TEST</div>
+			</Dropdown>
+			<Dropdown title="Productvoordelen">
+				<div>TEST</div>
+			</Dropdown>
+			<Dropdown title="Technische informatie">
+				<div>TEST</div>
+			</Dropdown>
+			<Dropdown title="Overige informatie">
+				<div>TEST</div>
+			</Dropdown>
+			<Dropdown title="Afbeeldingen">
+				<div>TEST</div>
+			</Dropdown>
+			<Dropdown title="Video">
+				<div>TEST</div>
+			</Dropdown>
+			<Dropdown title="Vergelijkbare producten">
+				<div>TEST</div>
+			</Dropdown>
+			<Dropdown title="Reviews">
+				<div>TEST</div>
+			</Dropdown>
+			<Dropdown title="Vragen & antwoorden">
+				<div>TEST</div>
+			</Dropdown>
+			<Dropdown title="Mijn laatst bekeken artikelen">
+				<div>TEST</div>
+			</Dropdown>
+			<Dropdown title="Diensten">
+				<div>TEST</div>
+			</Dropdown>
 		</div>
-	</article>
+	</div>
 {/await}
