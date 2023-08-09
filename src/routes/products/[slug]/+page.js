71cf6f63a445d3ac0b208a570a272de48ff42e60
Daniel Kadar <daniel.kadar@teamrockstars.nl>
2023-08-09 11:24:59 +0200

    moved to local state for queried products, made it async as well with loading indication

--- src/routes/products/[slug]/+page.js
+++ src/routes/products/[slug]/+page.js
@@ -1,19 +1,21 @@
-import { apiData } from '$stores/productStore';
+/**@typedef {import('$lib/types').Product} Product */
 
 /** @type {import('./$types').PageLoad} */
 export const load = ({ params }) => {
 	const url = `https://fakestoreapi.com/products/category/${params.slug}`;
 
-	fetch(url)
+	/** @type {Promise<Product[]>} */
+	const items = fetch(url)
 		.then((response) => response.json())
 		.then((data) => {
-			apiData.set(data);
+			return data;
 		})
 		.catch((error) => {
 			console.log(error);
 		});
 
 	return {
-		search: params.slug
+		search: params.slug,
+		items: { promise: items}
 	};
 };
--- src/routes/products/[slug]/+page.svelte
+++ src/routes/products/[slug]/+page.svelte
@@ -2,50 +2,53 @@
 	/** @type {import('./$types').PageData} */
 	export let data;
 
-	import { apiData } from '$stores/productStore';
-
+	import Loading from '$components/Loading.svelte';
 	import ProductCard from '$components/ProductCard.svelte';
 </script>
 
-<div>
-	<!-- search hero + result amount + categories -->
-	<div class="w-full bg-blue-400 p-1">
-		<div>
-			<div>Jouw zoekopdracht:</div>
-			<div>{data.search}</div>
-			<div>{$apiData.length} producten</div>
-		</div>
-		<div>
-			<div>Categorien bij je zoekopdracht</div>
-			<div />
-		</div>
-	</div>
-	<!-- amount + filter button (opens sidenav) (hidden on screens bigger than ) + sort -->
-	<!-- results -->
-	<div>
-		<section />
-		<section>
-			<nav class="flex">
-				<div>{$apiData.length} Artikelen</div>
-				<div><button>Filters</button></div>
-				<div>
-					<select>
-						<option>Onze selectie</option>
-						<option>Prijs laag - hoog</option>
-						<option>Prijs hoog - laag</option>
-						<option>Best beoordeeld</option>
-					</select>
-				</div>
-			</nav>
-			<div
-				class="grid grid-cols-2 border-l border-t border-gray-200 border-solid sm:grid-cols-3 lg:grid-cols-4"
-			>
-				{#each $apiData as product}
-					<ProductCard {...product} />
-				{/each}
+{#await data.items.promise}
+	<Loading />
+{:then items}
+	<div class="w-full">
+		<!-- search hero + result amount + categories -->
+		<div class="w-full bg-blue-400 p-1">
+			<div>
+				<div>Jouw zoekopdracht:</div>
+				<div>{data.search}</div>
+				<div>{items.length} producten</div>
 			</div>
-		</section>
+			<div>
+				<div>Categorien bij je zoekopdracht</div>
+				<div />
+			</div>
+		</div>
+		<!-- amount + filter button (opens sidenav) (hidden on screens bigger than ) + sort -->
+		<!-- results -->
+		<div>
+			<section />
+			<section>
+				<nav class="flex">
+					<div>{items.length} Artikelen</div>
+					<div><button>Filters</button></div>
+					<div>
+						<select>
+							<option>Onze selectie</option>
+							<option>Prijs laag - hoog</option>
+							<option>Prijs hoog - laag</option>
+							<option>Best beoordeeld</option>
+						</select>
+					</div>
+				</nav>
+				<div
+					class="grid grid-cols-2 border-l border-t border-gray-200 border-solid sm:grid-cols-3 lg:grid-cols-4"
+				>
+					{#each items as product}
+						<ProductCard {...product} />
+					{/each}
+				</div>
+			</section>
+		</div>
+		<!-- pagination -->
+		<!-- satisfaction level with page -->
 	</div>
-	<!-- pagination -->
-	<!-- satisfaction level with page -->
-</div>
+{/await}
