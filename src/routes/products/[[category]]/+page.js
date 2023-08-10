2b40aaef8626f95825f0e7ac73c5f1696c9b1b76
Daniel Kadar <daniel.kadar@teamrockstars.nl>
2023-08-10 17:37:03 +0200

    changed the way we resolve the promise for the products, added crude filtering and sorting to products array

--- src/routes/products/[[category]]/+page.js
+++ src/routes/products/[[category]]/+page.js
@@ -2,13 +2,8 @@
 
 /** @type {import('./$types').PageLoad} */
 export const load = ({ params }) => {
-	let url = '';
-	if (params.category) {
-		url = `https://fakestoreapi.com/products/category/${params.category}`;
-	} else {
-		url = 'https://fakestoreapi.com/products/';
-	}
-
+	const url = 'https://fakestoreapi.com/products/';
+	
 	/** @type {Promise<Product[]>} */
 	const items = fetch(url)
 		.then((response) => response.json())
@@ -19,6 +14,7 @@ export const load = ({ params }) => {
 			console.log(error);
 		});
 
+		
 	return {
 		search: params.category,
 		items: { promise: items }
--- src/routes/products/[[category]]/+page.svelte
+++ src/routes/products/[[category]]/+page.svelte
@@ -1,22 +1,53 @@
 <script>
-	/** @type {import('./$types').PageData} */
-	export let data;
-
 	import Loading from '$components/Loading.svelte';
 	import ProductCard from '$components/ProductCard.svelte';
 	import Sliders from '~icons/fa/sliders';
+
+	/** @type {import('./$types').PageData} */
+	export let data;
+	let loading = true;
+	/** @type {import('$lib/types').Product[] | undefined} */
+	let products;
+
+	data.items.promise.then((val) => {
+		loading = false;
+		products = val;
+	});
+
+	/** @type {string} */
+	let sortValue;
+	let filters = {
+		price: [-Infinity, Infinity],
+		rating: [-Infinity, Infinity]
+	};
+	$: productsToDisplay =
+		(products &&
+			products
+				.filter((product) => {
+					if (filters.price[0] > product.price) return false;
+					if (filters.price[1] < product.price) return false;
+
+					if (filters.rating[0] > product.rating.rate) return false;
+					if (filters.rating[1] < product.rating.rate) return false;
+
+					return true;
+				})
+				.sort((a, b) => {
+					return a.price - b.price;
+				})) ||
+		[];
 </script>
 
-{#await data.items.promise}
+{#if loading}
 	<Loading />
-{:then items}
+{:else}
 	<div class="w-full">
 		<!-- search hero + result amount + categories -->
 		<div class="w-full bg-blue-400 p-1">
 			<div>
 				<div>Jouw zoekopdracht:</div>
 				<div>{data.search}</div>
-				<div>{items.length} producten</div>
+				<div>{productsToDisplay.length} producten</div>
 			</div>
 			<div>
 				<div>Categorien bij je zoekopdracht</div>
@@ -32,7 +63,9 @@
 					class="grid grid-flow-col text-xs grid-cols-3 gap-2 min-h-[3rem] p-2 items-center justify-between"
 				>
 					<div>
-						<span class="border border-blue-950 p-1 px-2 rounded text-white bg-blue-950">{items.length}</span>
+						<span class="border border-blue-950 p-1 px-2 rounded text-white bg-blue-950"
+							>{productsToDisplay.length}</span
+						>
 						<span>Artikelen</span>
 					</div>
 					<button
@@ -42,18 +75,21 @@
 						<div class="absolute right-0 px-2"><Sliders /></div>
 					</button>
 					<div class="flex justify-center align-middle h-full">
-						<select class="w-full p-2.5 border-r-8 border-transparent outline outline-transparent">
-							<option selected>Onze selectie</option>
-							<option>Prijs laag - hoog</option>
-							<option>Prijs hoog - laag</option>
-							<option>Best beoordeeld</option>
+						<select
+							bind:value={sortValue}
+							class="w-full p-2.5 border-r-8 border-transparent outline outline-transparent"
+						>
+							<option selected value="">Onze selectie</option>
+							<option value="asc">Prijs laag - hoog</option>
+							<option value="desc">Prijs hoog - laag</option>
+							<!-- <option value="rating">Best beoordeeld</option> -->
 						</select>
 					</div>
 				</nav>
 				<div
 					class="grid grid-cols-2 border-l border-t border-gray-200 border-solid sm:grid-cols-3 lg:grid-cols-4"
 				>
-					{#each items as product}
+					{#each productsToDisplay as product}
 						<ProductCard {...product} />
 					{/each}
 				</div>
@@ -62,4 +98,4 @@
 		<!-- pagination -->
 		<!-- satisfaction level with page -->
 	</div>
-{/await}
+{/if}
