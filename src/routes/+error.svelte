da6928e4ece1238157ac95c729e6ddc32f95a2e0
Daniel Kadar <daniel.kadar@teamrockstars.nl>
2023-08-10 17:36:21 +0200

    added error page

---
+++ src/routes/+error.svelte
@@ -0,0 +1,15 @@
+<script>
+	// @ts-nocheck
+	import { page } from '$app/stores';
+
+    $: errorCategory = $page.status.toString()[0];
+</script>
+
+<div class="min-h-full min-w-full flex items-center align-middle justify-center">
+	{#if errorCategory === '4'}
+		<h1>Pagina kan niet worden geladen</h1>
+	{/if}
+	{#if errorCategory === '5'}
+		<h1>Serverfout</h1>
+	{/if}
+</div>
