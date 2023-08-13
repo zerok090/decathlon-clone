37b0ef1a0987279ccdb00d3762927efa830b9021
Daniel Kadar <daniel.kadar@teamrockstars.nl>
2023-08-13 13:53:43 +0200

    added loaded categories to navigationStore main page load

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,5 +1,28 @@
 <script>
+	import { onMount } from 'svelte';
+	import { categories } from '$stores/navigationStore';
 
+	onMount(() => {
+		fetch('https://fakestoreapi.com/products/categories')
+			.then((res) => res.json())
+			.then((json) => categories.set(json));
+	});
 </script>
 
-
+<div class="flex-1 mt-4 flex justify-center">
+	<div class="max-w-[121.6rem]">
+		<section />
+		<section class="w-full">
+			<ul class="capitalize grid grid-cols-2 sm:grid-cols-3 lg:grid-cols-5 gap-2">
+				<li class="flex justify-center w-full px-4 py-2 border border-black hover:bg-gray-200">
+					<a href="/products/">All</a>
+				</li>
+				{#each $categories as category}
+					<li class="flex justify-center w-full px-4 py-2 border border-black hover:bg-gray-200">
+						<a href={`/products/${category}`}>{category}</a>
+					</li>
+				{/each}
+			</ul>
+		</section>
+	</div>
+</div>
--- src/stores/navigationStore.js
+++ src/stores/navigationStore.js
@@ -1,12 +1,23 @@
-import { writable } from "svelte/store";
+import { writable } from 'svelte/store';
 
 function menuStore() {
 	const { subscribe, update } = writable(false);
 
 	return {
 		subscribe,
-		toggle: () => update((val) => !val),
+		toggle: () => update((val) => !val)
 	};
 }
 
 export const openMenu = menuStore();
+
+function categoryStore() {
+	const { subscribe, set } = writable([]);
+
+	return {
+		subscribe,
+		set,
+	}
+}
+
+export const categories = categoryStore();
