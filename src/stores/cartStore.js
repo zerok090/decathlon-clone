a3a2e5fe427192bad639a5132b5f22c22f62fa32
Daniel Kadar <daniel.kadar@teamrockstars.nl>
2023-08-13 21:39:22 +0200

    updated cart store to also store price not only amount of items

--- src/stores/cartStore.js
+++ src/stores/cartStore.js
@@ -4,21 +4,25 @@ import { fromLocalStorage, toLocalStorage } from '../client/localStorage';
 const storageKey = 'storedCart';
 
 function shoppingCart() {
-	/** @type {Map<import('$lib/types').ProductId, number>} */
+	/** @type {Map<import('$lib/types').ProductId, [number, number]>} */
 	const cart = fromLocalStorage(storageKey, new Map());
 	const { subscribe, update } = writable(cart);
 
-	/** @param {import('$lib/types').ProductId} id */
-	function addItem(id) {
+	/**
+	 *  @param {import('$lib/types').ProductId} id
+	 * @param {number} price
+	 */
+	function addItem(id, price) {
 		update((items) => {
 			const amount = items.get(id);
-			if (!amount) {
-				items.set(id, 1);
+			if (amount === undefined) {
+				items.set(id, [1, price]);
 			} else {
-				items.set(id, amount + 1);
+				items.set(id, [amount[0] + 1, amount[1]]);
 			}
 			return items;
 		});
+		console.log(cart);
 	}
 
 	/** @param {import('$lib/types').ProductId} id */
@@ -27,10 +31,10 @@ function shoppingCart() {
 			const amount = items.get(id);
 			if (!amount) {
 				return items;
-			} else if (amount === 1) {
+			} else if (amount[0] === 1) {
 				items.delete(id);
 			} else {
-				items.set(id, amount - 1);
+				items.set(id, [amount[0] - 1, amount[1]]);
 			}
 
 			return items;
@@ -60,7 +64,16 @@ export const amount = derived(cart, async ($cart) => {
 	let result = 0;
 
 	for (const value of $cart.values()) {
-		result += value;
+		result += value[0];
+	}
+
+	return result;
+});
+
+export const totalPrice = derived(cart, async ($cart) => {
+	let result = 0;
+	for (const value of $cart.values()) {
+		result += value[0] * value[1];
 	}
 
 	return result;
