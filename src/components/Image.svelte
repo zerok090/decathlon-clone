ae54a3386c16c67226738d46acd8539d3a0c998b
Daniel Kadar <daniel.kadar@teamrockstars.nl>
2023-08-21 15:22:37 +0200

    finished carousel component, added really basic image component for advanced config

---
+++ src/components/Image.svelte
@@ -0,0 +1,14 @@
+<script>
+	/** @type {string} */
+	export let alt;
+	/** @type {string} */
+	export let src;
+	/** @type {"lazy" | "eager" | null | undefined }*/
+	export let loading = 'lazy';
+	/** @type {"async" | "auto" | "sync" | null | undefined} */
+	export let decoding = 'async';
+</script>
+
+<picture class="w-full h-full">
+	<img class="object-cover w-full h-full" {src} {alt} {loading} {decoding} />
+</picture>
